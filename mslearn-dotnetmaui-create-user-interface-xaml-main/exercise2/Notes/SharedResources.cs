a7bd16f60d0752bd97c1a1c41d30bfcbaef2ebf2
rubene45 <rdiazmarey@gmail.com>
2023-11-21 23:59:41 +0100

    CABREO

---
+++ mslearn-dotnetmaui-create-user-interface-xaml-main/exercise2/Notes/SharedResources.cs
@@ -0,0 +1,7 @@
+﻿namespace Notes;
+
+static class SharedResources
+{
+    public static readonly Color FontColor = Color.FromRgb(0, 0, 0xFF);
+    public static readonly Color BackgroundColor = Color.FromRgb(0xFF, 0x8B, 0x8B);
+}
